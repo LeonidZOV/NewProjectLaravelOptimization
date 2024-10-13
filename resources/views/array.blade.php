3419af24453f360b38653b10f53df51cd2534d90
LeonidSZ <104792679+LeonidSZ@users.noreply.github.com>
2024-10-13 13:40:49 +0500

    2 task

---
+++ resources/views/array.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=".../css/app.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    @foreach ($array as $product)
+        <img src="{{$product['path']}}" alt="{{$product['title']}}" srcset="">
+        {{$product['title']}}
+        {{$product['price']}}
+    @endforeach
+
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="" alt="" srcset="">
+        <li>Главная</li>
+        <li>Массивы</li>
+    </header>
+    <h1>Главная страница</h1>
+    <ul>
+        <li>Ссылка 1</li>
+        <li>Ссылка 2</li>
+        <li>Ссылка 3</li>
+    </ul>
+    <footer> 
+        &copy; Зудихин Леонид Сергеевич
+    </footer>
+</body>
+</html>
